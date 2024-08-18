1cebaec419f4279fb00e7e496e95f52c1b3a8edc
Keks <keks@htmlacademy.ru>
2024-08-19 01:48:56 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import { generatePhotos } from './data.js';
+import './thumbnails.js';
+
+// Генерация массива фотографий
+const photos = generatePhotos();
+console.log(photos);
+
---
+++ 7/js/util.js
@@ -0,0 +1,20 @@
+/*Функция для проверки длины строки.*/
+/**
+ *
+ * @param {*} str - строка, которую нужно проверить
+ * @param {*} maxSymbols - максимальное количество символов
+ * @returns - возвращает true, если строка меньше или равна указанной длине,
+и false, если строка длиннее.
+ */
+const checkStringLength = (str = '', maxSymbols = 1) => str.length <= maxSymbols;
+//  пример вызова функции
+// console.log(checkStringLength('Функция для проверки длины строк', 30));
+//*  =========================================================================================
+
+// Генерируем случайное целое число в заданном диапазоне.
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// Возвращаем случайный элемент из переданного массива.
+const getRandomElement = (arr) => arr[getRandomInt(0, arr.length - 1)];
+
+export { getRandomInt, getRandomElement, checkStringLength };
