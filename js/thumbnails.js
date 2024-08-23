bc07adbcac53e464380afe29ffa456ac07fd3ed0
AtomWave <78145765+AtomWave@users.noreply.github.com>
2024-08-23 22:48:54 +0300

    вносит правки по дз 7.15

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,43 +1,47 @@
 import { MOCKED_PHOTOS as MOCKED_PHOTOS } from './example.js';
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
-const fragment = document.createDocumentFragment();
 
-const createThubnail = (photo) => {
-  const thambhail = template.cloneNode(true);
-  const image = thambhail.querySelector('.picture__img');
+
+const createThumbnail = (photo) => {
+  const thambnail = template.cloneNode(true);
+  const image = thambnail.querySelector('.picture__img');
   image.src = photo.url;
   image.alt = photo.description;
 
-  thambhail.querySelector('.picture__likes').textContent = photo.likes;
-  thambhail.querySelector('.picture__comments').textContent = photo.comments.length;
+  thambnail.querySelector('.picture__likes').textContent = photo.likes;
+  thambnail.querySelector('.picture__comments').textContent = photo.comments.length;
 
-  return thambhail;
+  return thambnail;
 };
 
 // *======== Вариант с циклом for ========
 
 /* for (let i = 0; i < MOCKED_PHOTOS.length; i++) {
   const photo = MOCKED_PHOTOS[i]; // Определяем photo как текущий элемент массива
-  const thambhail = createThubnail(photo);
-  fragment.appendChild(thambhail);
+  const thambnail = createThumbnail(photo);
+  fragment.appendChild(thambnail);
 };
 container.appendChild(fragment); */
 
 // *======== Вариант с for of ========
 
 /* for (const photo of MOCKED_PHOTOS) {
-  const thambhail = createThubnail(photo);
-  fragment.appendChild(thambhail);
+  const thambnail = createThumbnail(photo);
+  fragment.appendChild(thambnail);
 };
 container.appendChild(fragment); */
 
 
 // *======== Вариант с forEach ========
 
-MOCKED_PHOTOS.forEach((photo) => {
-  const thambhail = createThubnail(photo);
-  fragment.appendChild(thambhail);
-});
-container.appendChild(fragment);
+const renderCards = (data) => {
+  const fragment = document.createDocumentFragment();
+  data.forEach((photo) => {
+    const thumbnail = createThumbnail(photo);
+    fragment.appendChild(thumbnail);
+  });
+  container.appendChild(fragment);
+};
 
+export { renderCards }
