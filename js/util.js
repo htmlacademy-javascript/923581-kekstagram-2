00ac6c6ed7929eaf216167b96a53b00564d20728
AtomWave <AtomWave@yandex.ru>
2024-09-16 17:54:09 +0300

    добавил функцию numDecline

--- js/util.js
+++ js/util.js
@@ -26,14 +26,20 @@ const isEnterKey = (evt) => evt.key === 'Enter';
 // Обработка нажатий клавиш
 // Функция onDocumentKeydown обрабатывает нажатия клавиш и закрывает модальное окно при нажатии клавиш Escape или Enter.
 function onDocumentKeydown(evt) {
-  if (isEscapeKey(evt)) { // Проверяем нажатие клавиши Escape
+  if (isEscapeKey(evt) || isEnterKey(evt)) { // Проверяем, нажата ли клавиша Escape или Enter
     evt.preventDefault(); // Предотвращаем действие по умолчанию
-    closeModal(); // Закрываем модальное окно
-  } else if (isEnterKey(evt) && document.activeElement === imgUploadCancel) { // Проверяем нажатие клавиши Enter
-    evt.preventDefault();
     closeModal(); // Закрываем модальное окно
   }
 };
 
+const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
+  if (num % 10 === 1 && num % 100 !== 11) {
+    return nominative; // Если число оканчивается на 1, но не на 11
+  } else if (num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 12 || num % 100 > 14)) {
+    return genitiveSingular; // Если число оканчивается на 2, 3 или 4, но не на 12, 13 или 14
+  } else {
+    return genitivePlural; // Во всех остальных случаях
+  }
+};
 
-export { getRandomInt, getRandomElement, checkStringLength, isEnterKey, isEscapeKey, onDocumentKeydown };
+export { getRandomInt, getRandomElement, checkStringLength, isEnterKey, isEscapeKey, onDocumentKeydown, numDecline };
