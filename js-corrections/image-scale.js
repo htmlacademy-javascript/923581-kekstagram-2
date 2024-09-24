64d9297337b642f32187ebe584dc9dcb7c7542aa
AtomWave <AtomWave@yandex.ru>
2024-09-24 12:50:52 +0300

    удаляет временную папку

--- js-corrections/image-scale.js
+++
@@ -1,5 +0,0 @@
-// image-scale.js
-import { updateScale, onSmallerClick, onBiggerClick } from './image-utils.js';
-
-// Экспортируем функции для масштабирования
-export { updateScale, onSmallerClick, onBiggerClick };
