58ad95bc0edb60a767bbff759c307c47076d4674
AtomWave <78145765+AtomWave@users.noreply.github.com>
2024-10-03 23:22:34 +0300

    реализует «устранения дребезга» для фильтров

--- js/util.js
+++ js/util.js
@@ -1,18 +1,9 @@
 import { closeModal } from './photo-modal.js';
 
-// Функция для проверки длины строки
 const checkStringLength = (str = '', maxSymbols = 1) => str.length <= maxSymbols;
-
-// Функция для генерации случайного целого числа в заданном диапазоне
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-// Функция для получения случайного элемента из массива
 const getRandomElement = (arr) => arr[getRandomInt(0, arr.length - 1)];
-
-// Функция для проверки, была ли нажата клавиша Escape
 const isEscapeKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
-
-// Функция для проверки, была ли нажата клавиша Enter
 const isEnterKey = (evt) => evt.key === 'Enter';
 
 // Обработчик события на нажатие клавиши на всем документе
@@ -37,14 +28,15 @@ const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
 const ALERT_SHOW_TIME = 5000;
 
 const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
-// const titleElement = document.querySelector('#data-error').content.querySelector('.data-error__title');
+const titleElement = document.querySelector('#data-error').content.querySelector('.data-error__title');
 
 const showAlert = (message) => {
   if (!dataErrorTemplate) {
-    return; // Выход, если шаблон не найден
+    return;
   }
 
   const popup = dataErrorTemplate.cloneNode(true);
+  titleElement.textContent = message;
   document.body.append(popup);
 
   setTimeout(() => {
@@ -52,33 +44,9 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-// const showAlert = (message) => {
-//   if (!dataErrorTemplate) {
-//     return; // Выход, если шаблон не найден
-//   }
-
-//   const popup = dataErrorTemplate.cloneNode(true);
-
-//   // Добавляем сообщение в элемент
-//   const messageElement = popup.querySelector('.data-error__title'); // Предполагается, что у вас есть элемент для сообщения
-//   if (messageElement) {
-//     messageElement.textContent = message; // Устанавливаем текст сообщения
-//   }
-
-//   document.body.append(popup);
-
-//   setTimeout(() => {
-//     popup.remove();
-//   }, ALERT_SHOW_TIME);
-// };
-
-
 // Функция для получения случайных изображений
 function getRandomImages(arr, count) {
-  // Перемешивание массива
   const shuffled = arr.sort(() => 0.5 - Math.random());
-
-  // Возврат первых count элементов из перемешанного массива
   return shuffled.slice(0, count);
 }
 
@@ -91,5 +59,6 @@ export {
   onDocumentKeydown,
   numDecline,
   showAlert,
-  getRandomImages
+  getRandomImages,
+  titleElement
 };
