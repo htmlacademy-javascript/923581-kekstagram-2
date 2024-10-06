1ee12d029c92f2af2ecb95dd80dba0666e39de74
AtomWave <AtomWave@yandex.ru>
2024-10-06 19:55:50 +0300

    удаляет вызовы сообщения в консоль

--- js/photo-upload.js
+++ js/photo-upload.js
@@ -7,9 +7,7 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-  console.log('object');
   if (matches) {
-    console.log('object');
     preview.src = URL.createObjectURL(file);
   }
 });
